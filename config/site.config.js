9b70d4d076a4c66a9151877483b978f34a5621fe
wvclouds <138031353+wvclouds@users.noreply.github.com>
2024-11-19 14:02:10 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -14,7 +14,7 @@ module.exports = {
   icon: '/icons/wvclouds_rmbg.png',
 
   // The name of your website. Present alongside your icon.
-  title: "WVCLOUDS's OneDrive02-CF-Index",
+  title: "OneDrive02-CF-Index",
 
   // The folder that you are to share publicly with onedrive-cf-index-ng. Use '/' if you want to share your root folder.
   baseDirectory: process.env.BASE_DIRECTORY || '/',
